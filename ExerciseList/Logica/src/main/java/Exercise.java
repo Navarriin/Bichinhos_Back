5612f34e651363679c6aad315aeda45ba0c26372
Gabriel <gabrielnavarro.vip@gmail.com>
2024-05-23 23:51:01 -0300

    Feat: Resolução do exercicio de lógica.

---
+++ ExerciseList/Logica/src/main/java/Exercise.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Exercise {
+    public static void main(String[] args) {
+
+        System.out.println("Olá!!");
+        System.out.println("Insira uma lista numérica, separando cada elemento por ',': ");
+
+        List<Integer> listOfNumbers = receivingListOfNumbersAndConvert();
+
+        sortedListAndPrint(listOfNumbers);
+    }
+
+    private static List<Integer> receivingListOfNumbersAndConvert() {
+        Scanner scan = new Scanner(System.in);
+        List<Integer> listOfNumbers = new ArrayList<>();
+        String[] numbers = scan.nextLine().split(",");
+
+
+        for(String number : numbers) {
+            try {
+                int numberConvert = Integer.parseInt(number.trim());
+                listOfNumbers.add(numberConvert);
+            } catch (NumberFormatException exception) {
+                System.out.printf("%s não é um número válido. \n", number);
+            }
+        }
+        return listOfNumbers;
+    }
+
+    private static void sortedListAndPrint(List<Integer> list) {
+        Collections.sort(list);
+
+        System.out.println("Sua lista de números válidos ordenada de forma crescente: ");
+        for (int i = 0; i < list.size(); i++) {
+            if(i == list.size() - 1) {
+                System.out.print(list.get(i));
+            } else {
+                System.out.print(list.get(i) + ",");
+            }
+        }
+    }
+}
